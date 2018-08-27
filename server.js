4314618976c28a997c0b9c3e13e08346c00dac77
Scott Rock <scottrockbiz@gmail.com>
2018-08-27 11:11:03 -0700

    adding and displaying working, started formatting

--- server.js
+++ server.js
@@ -6,6 +6,36 @@ const express = require('express'),
       { Schema } = mongoose,
       app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb://localhost/chipmunks',
+  { useNewUrlParser: true }
+);
+mongoose.connection.on('connected', () => console.log('Mongo DB connected'));
+
+// create and register chipmunk schema
+const chipmunkSchema = new Schema({
+  name: {
+    type: String,
+    required: [ true, 'A chipmunk needs a name' ],
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: [ true, 'How old is this chipmunk' ],
+  },
+  favoriteNut: {
+    type: String,
+    trim: true,
+    default: 'acorn',
+  },
+  treasure: {
+    type: String,
+    trim: true,
+  },
+});
+const Chipmunk = mongoose.model('Chipmunk', chipmunkSchema);
+
 // server set up
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('views'));
@@ -14,7 +44,22 @@ app.use(parser.urlencoded({ extended: true }));
 
 // routes
 app.get('/', function(request, response) {
-  response.render('index');
+  Chipmunk.find({})
+    .then( chipmunks => {
+      console.log(chipmunks);
+      response.render('index', { chipmunks });
+    })
+    .catch(console.log);
+});
+
+app.get('/chipmunks/new', function(request, response) {
+  response.render('chipmunks/new');
+});
+
+app.post('/chipmunks', function(request, response) {
+  Chipmunk.create(request.body)
+    .then(chipmunk => response.redirect('/'))
+    .catch(console.log);
 });
 
 // start server
